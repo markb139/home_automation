fee7594d3f73566a12f2194cfc7a8ccfb3c9c822
Mark Burton <mark.burton@pinpoint-software.com>
2022-10-23 11:26:37 +0100

    add retry for energenie switch

--- automation/message_handler.py
+++ automation/message_handler.py
@@ -4,35 +4,65 @@ from paho.mqtt.publish import single
 
 
 class MessageHandler:
-	def __init__(self):
-		self.mqttc = Client()
-		self.mqttc.on_subscribe = self.on_subscribe
-		self.mqttc.on_message = self.on_message
-		self.mqttc.connect("127.0.0.1", 1883, 60)
-		self.mqttc.subscribe("home_automation/switch1/power_on", 0)
+    def __init__(self):
+        self.mqttc = Client()
+        self.mqttc.on_subscribe = self.on_subscribe
+        self.mqttc.on_message = self.on_message
+        self.mqttc.connect("127.0.0.1", 1883, 60)
+        self.mqttc.subscribe("home_automation/switch1/power_on", 0)
+        self.expected_value = None
+        self.retry_count = 0
 
-	def set_switch(self, _switch):
-		self._switch = _switch
-	
-	def handle(self, latest):
-		print(f'{latest}')
+    def set_switch(self, _switch):
+        self._switch = _switch
 
-		try:
-			single(topic='home_automation/switch1', payload=json.dumps(latest), hostname='127.0.0.1')
-		except Exception as err:
-			print(err)
-			print(latest)
-  
-	def on_message(self, mqttc, obj, msg):
-		if msg.payload == b'true':
-			print('ON')
-			self._switch.turn_on()
-		else:
-			print('OFF')
-			self._switch.turn_off()
+    def handle(self, latest):
+        print(f'{latest} expected {self.expected_value} {self.retry_count}')
 
-	def on_subscribe(self, mqttc, obj, mid, granted_qos):
-		print("Subscribed: " + str(mid) + " " + str(granted_qos))
+        try:
+            single(topic='home_automation/switch1', payload=json.dumps(latest), hostname='127.0.0.1')
+        except Exception as err:
+            print(err)
+            print(latest)
 
-	def loop(self):
-		self.mqttc.loop()
+        if self.expected_value is not None:
+            try:
+                if latest["switch"] == False:
+                    if self.expected_value == True:
+                        print("Retry - ON")
+                        self._switch.turn_on()
+                        self.retry_count += 1
+                    else:
+                        self.expected_value = None
+                        self.retry_count = 0
+                elif latest["switch"] == True:
+                    if self.expected_value == False:
+                        print("Retry - OFF")
+                        self._switch.turn_off()
+                        self.retry_count += 1
+                    else:
+                        self.expected_value = None
+                        self.retry_count = 0
+                else:
+                        print("Unexpected value of switch")
+            except Exception as err:
+                print(err)
+                print(latest)
+
+    def on_message(self, mqttc, obj, msg):
+        if msg.payload == b'true':
+            print('ON')
+            self._switch.turn_on()
+            self.expected_value=True
+            self.retry_count = 0
+        else:
+            print('OFF')
+            self._switch.turn_off()
+            self.expected_value=False
+            self.retry_count = 0
+
+    def on_subscribe(self, mqttc, obj, mid, granted_qos):
+        print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+    def loop(self):
+        self.mqttc.loop()
